d944e989125bfdfc8a7865b25a9e29d78449db95
jandersonpaixao <janderson.paixao.a@gmail.com>
2022-06-08 23:10:09 -0300

    Feat: implementação do put e delete de postagens

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -19,10 +19,24 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://blogpessoaljandersonpaixao.herokuapp.com/postagens',this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://blogpessoaljandersonpaixao.herokuapp.com/postagens/${id}`,this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://blogpessoaljandersonpaixao.herokuapp.com/postagens', postagem ,this.token)
   }
 
-  
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://blogpessoaljandersonpaixao.herokuapp.com/postagens', postagem ,this.token)
+  }
+
+  deletePostagem(id: number){
+     return this.http.delete(`https://blogpessoaljandersonpaixao.herokuapp.com/postagens/${id}`,this.token)
+  }
 
 }
+
+  
+
+
