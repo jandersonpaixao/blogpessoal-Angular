181bc4c21cd6a117ba6d5285f82413f8ed39b8e8
jandersonpaixao <janderson.paixao.a@gmail.com>
2022-06-08 22:08:24 -0300

    Feat: implementação do post e get de postagem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -24,6 +24,10 @@ export class AuthService {
 
   }
 
+  getByIdUsuario(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://blogpessoaljandersonpaixao.herokuapp.com/usuarios/${id}`)
+  }
+
   logado(){
     let ok: boolean = false
 
